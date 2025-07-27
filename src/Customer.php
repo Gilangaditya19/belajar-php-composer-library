c3fb7afcfb8f9a1d0228004116bb8c1b2dc6f601
Gilang Aditya <langzaditya21@gmail.com>
2025-07-27 19:15:02 +0700

    Add Default Value

--- src/Customer.php
+++ src/Customer.php
@@ -6,7 +6,7 @@ class Customer
 {
     public function __construct(private string $name) {}
 
-    public function sayHello(string $name)
+    public function sayHello(string $name = "Guest"): string
     {
         return "Hello $name My Name is $this->name";
     }
